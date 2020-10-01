f0527b4585fae58e23c80e1a0547075e4e527f13
PhamVanTien <tien.phamvan@gameloft.com>
2020-10-01 15:54:57 +0700

    rotate cute around

--- Assets/Script/CuteController.cs
+++ Assets/Script/CuteController.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class CuteController : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+     
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        this.transform.Rotate(0,20*Time.deltaTime,0);
     }
 }
