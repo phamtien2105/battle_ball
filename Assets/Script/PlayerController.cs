1fc6a791bc3aac8011d06b89457837016f0ec4ba
PhamVanTien <tien.phamvan@gameloft.com>
2020-10-02 16:51:57 +0700

    add player and pickup

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Rigidbody body;
+    public float speed;
+    void Start()
+    {
+       body  = this.GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 moment= new Vector3(moveHorizontal,0.0f,moveVertical);
+        body.AddForce(moment*speed);
+      
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
