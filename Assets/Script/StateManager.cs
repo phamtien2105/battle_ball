0b8d987f3c611cbedb6225305d5715c119cdcb9f
PhamVanTien <tien.phamvan@gameloft.com>
2020-10-13 18:10:06 +0700

    add inActiveAnimation for layer vs enermy

---
+++ Assets/Script/StateManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("change alpha");
+     
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
