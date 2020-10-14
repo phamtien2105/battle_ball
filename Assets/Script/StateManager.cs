550114e8fd1a6af878a49bca25058cce75888a48
PhamVanTien <tien.phamvan@gameloft.com>
2020-10-14 11:36:00 +0700

    remove redudant code

--- Assets/Script/StateManager.cs
+++ Assets/Script/StateManager.cs
@@ -7,20 +7,14 @@ public class StateManager : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public Text CountText;
-    private float timer;
     void Start()
     {
-        Debug.Log("change alpha");
-        CountText.text = "0s";
-        timer = +Time.deltaTime;
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        // update timer text
-        timer = +Time.deltaTime;
-        CountText.text = (int)timer + "s";
+    
     }
 }
