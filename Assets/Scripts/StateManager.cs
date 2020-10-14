7d041f17159154d4b5dfa41ba6a4a13a6068daaf
PhamVanTien <tien.phamvan@gameloft.com>
2020-10-14 22:33:12 +0700

    add keyfream eanble detect area for defend

--- Assets/Scripts/StateManager.cs
+++ Assets/Scripts/StateManager.cs
@@ -7,41 +7,66 @@ public class StateManager : MonoBehaviour
 {
     // Start is called before the first frame update
 
+    public enum EnumMode { Attack, Defend };
     public float CostEnergy;
     public float InActiveTime;
 
     public static bool isBallAvaiable;
     public static Vector3 BallPosition;
-    
+
+    private EnumMode MyEnumMode;
+
+
 
     void Start()
     {
-        StartCoroutine("changeToDefendState", InActiveTime);
 
+        MyEnumMode = EnumMode.Defend;
+        StartCoroutine("onInactiveState", InActiveTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isBallAvaiable)
+        if (isBallAvaiable && MyEnumMode == EnumMode.Attack)
             chaseBall();
+
     }
 
 
-    IEnumerator changeToDefendState(int time)
+    IEnumerator onInactiveState(int time)
     {
         yield return new WaitForSeconds(time);
-        gameObject.GetComponent<Animator>().SetTrigger("isDefend");
-        //enable detect area
-        gameObject.transform.Find("DetectArea").gameObject.SetActive(true);
+
+        if (MyEnumMode == EnumMode.Defend)
+        {
+
+            gameObject.GetComponent<Animator>().SetTrigger("isDefend");
+
+        }
     }
 
     public void chaseBall()
     {
-        transform.position = Vector3.MoveTowards(transform.position,
-                   BallPosition, 1.5f * Time.deltaTime);
-        Vector3 rotationDestination = BallPosition;
-        Quaternion targetRotation = Quaternion.LookRotation(rotationDestination - transform.position, Vector3.up);
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 6.0f);
+        if (GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("isChase"))
+        {
+            transform.position = Vector3.MoveTowards(transform.position,
+                       BallPosition, 1.5f * Time.deltaTime);
+            Vector3 rotationDestination = BallPosition;
+            Quaternion targetRotation = Quaternion.LookRotation(rotationDestination - transform.position, Vector3.up);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 6.0f);
+        }
+    }
+
+    public void EnableDetectArea()
+    {
+        //enable detect area
+        if (MyEnumMode == EnumMode.Defend)
+            gameObject.transform.Find("DetectArea").gameObject.SetActive(true);
+    }
+
+    public void setEnumMode(EnumMode mode)
+    {
+        this.MyEnumMode = mode;
     }
 }
