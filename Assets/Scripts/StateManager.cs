5896cda1ebc1517e6938a8a89f3ea1171a4d6778
PhamVanTien <tien.phamvan@gameloft.com>
2020-10-14 18:54:55 +0700

    add code for actived to defend on 0.5 time

--- Assets/Scripts/StateManager.cs
+++ Assets/Scripts/StateManager.cs
@@ -8,15 +8,26 @@ public class StateManager : MonoBehaviour
     // Start is called before the first frame update
 
     public float CostEnergy;
+    public float InActiveTime;
 
     void Start()
     {
-       
+        StartCoroutine("changeToDefendState", InActiveTime);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-    
+
+    }
+
+
+    IEnumerator changeToDefendState(int time)
+    {
+        yield return new WaitForSeconds(time);
+        gameObject.GetComponent<Animator>().SetTrigger("isDefend");
+        //enable detect area
+        gameObject.transform.Find("DetectArea").gameObject.SetActive(true);
     }
 }
